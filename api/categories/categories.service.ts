85adfb75b31f4a3c61c8d1617f604d619e3626ac
migueljo <migueljomartinez@gmail.com>
2023-06-13 23:17:49 -0500

    Refactoring

--- api/categories/categories.service.ts
+++ api/categories/categories.service.ts
@@ -1,4 +1,3 @@
-import { faker } from '@faker-js/faker';
 import * as Boom from '@hapi/boom';
 import { v4 as uuid } from 'uuid';
 
@@ -6,25 +5,6 @@ import { Category } from './categories.schema.js';
 import { CategoryModel } from './categories.model.js';
 
 export class CategoriesService {
-  private categories;
-
-  constructor() {
-    this.categories = this.generate();
-  }
-
-  private generate(size = 100): Category[] {
-    // TODO: Use real DB
-    const first: Category = {
-      name: 'First category',
-      id: '9836cd0b-d90b-4af7-b485-5d1ded8db245',
-    };
-    const categories: Category[] = [...Array(size)].map(() => ({
-      name: faker.word.noun(),
-      id: faker.string.uuid(),
-    }));
-    return [first, ...categories];
-  }
-
   async create(category: Omit<Category, 'id'>): Promise<Category> {
     const categoryCreated = await CategoryModel.create({
       ...category,
