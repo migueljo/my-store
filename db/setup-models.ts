9fd14bfdfab9bcec69925bc2a8ec3d98aedca22b
migueljo <migueljomartinez@gmail.com>
2023-06-27 00:14:49 -0500

    Removing logs

--- db/setup-models.ts
+++ db/setup-models.ts
@@ -16,22 +16,15 @@ import {
 
 export function setupModels(sequelize: Sequelize): void {
   try {
-    console.log('setupModels() - before init');
     UserModel.init(UserModelSchema, UserModel.config(sequelize));
-    console.log('setupModels() - after init UserModel');
     CustomerModel.init(CustomerModelSchema, CustomerModel.config(sequelize));
-    console.log('setupModels() - after init CustomerModel');
     ProductModel.init(ProductModelSchema, ProductModel.config(sequelize));
-    console.log('setupModels() - after init ProductModel');
     CategoryModel.init(CategoryModelSchema, CategoryModel.config(sequelize));
-    console.log('setupModels() - after init CategoryModel');
 
     CustomerModel.associate(sequelize);
     UserModel.associate(sequelize);
     ProductModel.associate(sequelize);
     CategoryModel.associate(sequelize);
-
-    console.log('setupModels() - after init');
   } catch (error) {
     console.log('setupModels() - error', error);
   }
