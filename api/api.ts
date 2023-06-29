742c5aae9fde2987e4a000f9d7cf42d8013df6c4
migueljo <migueljomartinez@gmail.com>
2023-06-28 22:09:59 -0500

    Registering orders router

--- api/api.ts
+++ api/api.ts
@@ -4,6 +4,7 @@ import { productsRouter } from './products/products.router.js';
 import { categoriesRouter } from './categories/categories.router.js';
 import { usersRouter } from './users/users.router.js';
 import { customersRouter } from './customer/customers.router.js';
+import { ordersRouter } from './orders/orders.router.js';
 
 export function routerApi(app: Express): void {
   const router = express.Router();
@@ -12,4 +13,5 @@ export function routerApi(app: Express): void {
   router.use(categoriesRouter);
   router.use(usersRouter);
   router.use(customersRouter);
+  router.use(ordersRouter);
 }
